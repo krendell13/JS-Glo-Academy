e3f7f3845107d74e1243415934a9f444e038d502
krendell13 <ilove13@internet.ru>
2022-01-30 12:49:19 +0300

    Выполнили первое задание

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Урок 1. Введение в язык, подключение javascript. Основы работы с Git.
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('Всем привет!');
+
+console.log('Учится в GLO ACADEMY без куратора - это весело!');
