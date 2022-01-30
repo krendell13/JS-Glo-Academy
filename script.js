3bed88db83203709c146f761bda43b98ec6b694d
krendell13 <ilove13@internet.ru>
2022-01-30 20:16:02 +0300

    Выполнено второе задание

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// Урок 1. Введение в язык, подключение javascript. Основы работы с Git.
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+// Урок 2. Типы данных, операторы, методы и свойства
 
-alert('Всем привет!');
+// название проекта
+const title = "Калькулятор доходов";
+// название типов экранов
+const screens = "Простые, Сложные, Интерактивные";
+// любое число
+const screenPrice = 15;
+// % отката посреднику - любое число от 1 до 100;
+const rollback = 90;
+// любое число (сколько хотите заработать)
+const fullPrice = 1000000;
+// булевое значение
+const adaptive = true;
 
-console.log('Учится в GLO ACADEMY без куратора - это весело!');
+console.log(`Типы переменных: \n ` +
+  `\t title: ${typeof(title)}, fullPrice: ${typeof(fullPrice)}, adaptive: ${typeof(adaptive)}`);
+console.log("длина строки screens = "+ screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+// строку screens в нижний регистр и разбить на массив и вывести его в консоль
+console.log( screens.toLowerCase().split(', ') );
+// откат посреднику
+console.log(`Посреднику - ${rollback}% \nЭто целых ${ fullPrice * (rollback/100) } рублей/ долларов/гривен/юани !`);
